941822f3f21418139014874e53d94183f53d423d
jacalynli <jacalynalice@gmail.com>
2016-09-02 11:22:03 -0700

    Added files

---
+++ APLesson_01/ASCII2.java
@@ -0,0 +1,20 @@
+public class ASCII2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                       _____");
+		System.out.println("                    /    ___    \\");
+		System.out.println("	   /    /  _  \\    \\");
+		System.out.println("	 /(  /( /(_)\\ )\\  )\\");
+		System.out.println("               (    \\  \\ ___ /  /    )");
+		System.out.println("               (      \\ _____ /     )");
+		System.out.println("               /(                       )\\");
+		System.out.println("             |    \\                    /   |");
+		System.out.println("             |      \\ _______ /     |");
+		System.out.println("              \\    /     \\      /    \\    /");
+		System.out.println("                \\/         |   |         \\/");
+		System.out.println("                            |   |");
+		System.out.println("                            |   |");
+		System.out.println("                            |_ |");
+	}
+}
---
+++ APLesson_01/HELLOWORLD.java
@@ -0,0 +1,12 @@
+public class HELLOWORLD
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  ___  _____________.____        .____        ________    ");		
+		System.out.println(" /      |     \\_      _____/|        |      |        |     \\_____       \\   ");
+		System.out.println("/       ~     \\         __)_  |        |      |        |        /      |        \\  "); 
+		System.out.println("\\       Y      /                \\|         |___|        |___/       |          \\ ");
+		System.out.println(" \\___|_   /_______   / |_______ \\_______  \\_______    /");
+		System.out.println("            \\/               \\/                  \\/              \\/                \\/   ");
+	}
+}
