4efcc648678b0cbb98c7b105fdb64d991639e4ee
jacalynli <jacalynalice@gmail.com>
2016-09-13 11:46:41 -0700

    Finish Lesson 3

--- APLesson_03/EX_03.java
+++ APLesson_03/EX_03.java
@@ -7,9 +7,13 @@ public class EX_03
 	//instaniate the new Scanner object "keyboard"
 	Scanner keyboard = new Scanner(System.in);
 	
-	//Self-intro
-	System.out.println("Hello! I convert from fahrenheit to celsius. What number would you like to convert?");
+	//Self-intro and prompt for user input
+	System.out.println("Hello! I convert from fahrenheit to celsius. What temperature would you like to convert?");
 	
-	//
+	//Search for the number that user enters
+	int f = keyboard.nextInt();
+	
+	//Print the result
+	System.out.println(f + " in celsius is " + ((f - 32) * 5 / 9) + ".");
 	}
 }
