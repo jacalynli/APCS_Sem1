796834f1936d4c027954772bddcbebc39aa0c171
jacalynli <jacalynalice@gmail.com>
2016-10-11 12:13:25 -0700

    Lesson 5.1

---
+++ APLesson_05/5.1/EX03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+public class EX03
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Enter your height: ");
+		System.out.println("Enter your weight: ");
+		
+		
+	}
+	
+	public static void 
+}
---
+++ APLesson_05/5.1/Equals.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Equals
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a word: ");
+		String word1 = keyboard.next();
+		String word2 = keyboard.next();
+		
+		if(word1.equals(word2))
+			System.out.println("The words are equal!");
+		else
+			System.out.println("The words are not equal!");
+	}
+}
