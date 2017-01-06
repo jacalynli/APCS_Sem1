014554fb1ab17c8a22aa7ce29d29d4c8d6ea44eb
jacalynli <jacalynalice@gmail.com>
2017-01-06 10:40:32 -0800

    Lesson 11

--- APLesson_11/Ex_04.java
+++ APLesson_11/Ex_04.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Ex_04
 {
 	public static void main(String[]args)
@@ -17,8 +18,25 @@ public class Ex_04
 		}
 		
 		// 3. User prompt: “Please enter a number: ”
+		
+		System.out.println("Please enter a number: ");
 		//4. Create a new int called “divisor” = user input
+		
+		Scanner keyboard = new Scanner (System.in);
+		int divisor = keyboard.nextInt();
+	
 
 		//5. Create another new int called “count”, set = 0
+		int count = 0;
+		for (int i = 0; i < nums.length; i++)
+		{
+			for(int j = 0; j < nums[i].length; j++)
+				if (nums [i][j] % divisor == 0)
+				{
+					count++;
+				}
+		}
+		
+		System.out.println("There are " + count + " numbers divisible by " + divisor + " in the array.");
 	}
 }
---
+++ StudyGuide.java
@@ -0,0 +1,19 @@
+public class StudyGuide
+{
+	public static void main(String[]args)
+	{
+		//Question 1
+		int num = 45;
+		System.out.println("IronMan has " + num + " kinds of weapons in his suit.");
+		
+		//Question 2
+		String pres = "Abraham Lincoln";
+		String quote = "Whatever you are, be a good one.";
+
+		System.out.println(pres + " once said, \"" + quote + "\"");
+		
+		//Question
+		System.out.println("a\tb\tc\nd\te\tf\ng\th\ti");
+	
+	}
+}
